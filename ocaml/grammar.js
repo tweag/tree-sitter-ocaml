efe17d42acc28b446cb4aa80874d04d1a968e5ad
GuillaumeGen <guillaume.genestier@tweag.io>
2022-12-12 18:26:01 +0100

    The real test

--- ocaml/grammar.js
+++ ocaml/grammar.js
@@ -2026,8 +2026,8 @@ module.exports = grammar({
 
     _label: $ => seq(choice('~', '?'), $._label_name),
     directive: $ => seq('#', choice($._identifier, $._capitalized_identifier)),
-    type_variable: $ => seq("'", choice($._identifier, $._capitalized_identifier)),
-    tag: $ => seq('`', choice($._identifier, $._constructor_name)),
+    type_variable: $ => seq("'", choice($._type_constructor)),
+    tag: $ => seq('`', choice($._constructor_name)),
     attribute_id: $ => sep1('.', choice($._identifier, $._capitalized_identifier))
   },
 
